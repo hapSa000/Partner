1f8e5665415e82439d0429d41195b4e852cc5458
pushpendrachouhan998 <pushpendrachouhan998@gmail.com>
2023-02-02 16:44:54 +0530

    Project Setup Done

---
+++ src/screens/appIntroScreen/AppIntro.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AppIntro = () => {
+  return (
+    <View>
+      <Text>AppIntro</Text>
+    </View>
+  )
+}
+
+export default AppIntro

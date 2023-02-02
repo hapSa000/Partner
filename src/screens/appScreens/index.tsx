bbaff0f985c493f5a1b06880ba561ad6c5835b02
pushpendrachouhan998 <pushpendrachouhan998@gmail.com>
2023-02-02 16:02:06 +0530

    Project Setup basic

---
+++ src/screens/appScreens/index.tsx
@@ -0,0 +1,4 @@
+export {default as Login} from './login';
+export {default as ForgotPassword} from './forgotPassword';
+export {default as ResetPassword} from './resetPassword';
+export {default as SignUp} from './singup';

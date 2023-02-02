3eab967ce2663e955e21eeb411c624f60846ef1b
pushpendrachouhan998 <pushpendrachouhan998@gmail.com>
2023-02-02 16:21:02 +0530

    Project Setup basic with color code and constants

---
+++ src/constants/colorConstsnts/colors.tsx
@@ -0,0 +1,12 @@
+const colors = {
+  Black: 'rgb(0, 0, 0)',
+  Red: 'rgb(249, 4, 4)',
+  Green: 'rgb(4, 249, 8)',
+  Blue: 'rgb(7, 21, 251 )',
+  Orange: 'rgb(253, 106, 75)',
+  White: 'rgb(255, 255, 255)',
+  Pink: 'rgb(244, 7, 251)',
+  Brown: 'rgb(251, 173, 7)',
+};
+
+export default colors;

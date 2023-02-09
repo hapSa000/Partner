c2a7399bee1afe5613094d82510f7e81d3d62f0f
sanjeevgaur797 <sanjeevgaur797@gmail.com>
2023-02-09 23:58:49 +0530

    font setup

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import {View, Text} from 'react-native';
 import React from 'react';
 
 import AppIntro from './src/screens/appIntroScreen/AppIntro';
-import {ForgotPassword} from './src/screens/authScreens';
+import {ForgotPassword, TermsAndCondition} from './src/screens/authScreens';
 import Step1 from './src/screens/ProfileSetup/Step1';
 
 const App = () => {
-  return <Step1 />;
+  return <TermsAndCondition />;
 };
 
 export default App;

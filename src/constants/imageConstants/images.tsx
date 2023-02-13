6707a39a7dc9620a2dd871df481cc08a8dd187da
bhattash777 <99108933+bhattash777@users.noreply.github.com>
2023-02-14 00:25:49 +0530

    Ash sec. push

--- src/constants/imageConstants/images.tsx
+++ src/constants/imageConstants/images.tsx
@@ -1,7 +1,8 @@
 const images = {
     tinder: require('../../assets/images/tinder.png'),
-    google:require('../../assets/images/google.png'),
-    facebook:require('../../assets/images/facebook.png'),
-    speechBubble:require('../../assets/images/speechBubble.png')
+    google: require('../../assets/images/google.png'),
+    facebook: require('../../assets/images/facebook.png'),
+    speechBubble: require('../../assets/images/speechBubble.png'),
+    Logo: require('../../assets/images/logo.png')
 }
 export default images;
